e1846f296c0586cc2296c35c86915538bf70696b
NguyenAnhTuan <tuanquynh1111050102@gmail.com>
2024-01-03 19:06:01 +0700

    update IntroductionandGoal

--- FE/src/Pages/Home/HomeIntroduction.jsx
+++ FE/src/Pages/Home/HomeIntroduction.jsx
@@ -10,10 +10,10 @@ function HomeIntroduction() {
     <div className={Styles.introdction_seeMore}>
         <a href="">See more<FaChevronRight/></a>
     </div>
-    <div className={Styles.HomeIntroduction_List}>
-    <HomeIntroductionList boxcolor='red' text_des='Context'/>
-    <HomeIntroductionList boxcolor='Blue' text_des='Hello'/>
-    <HomeIntroductionList/>
+    <div className={Styles.HomeIntroduction_List} style={{fontWeight:'bold'}}>
+    <HomeIntroductionList boxcolor='red' text_des='Chào mừng đến với SkinDiagnoTech'/>
+    <HomeIntroductionList boxcolor='Blue' text_des='SkinDiagnoTech cung cấp hệ thống chuẩn đoán bệnh da liễu thông qua hình ảnh và tạo ra một diễn đàn để mọi người cùng nhau giải đáp những thắc mắc.'/>
+    <HomeIntroductionList text_des='Đặt sức khỏe của người bệnh lên hàng đầu'/>
     </div>
     </>
   )
